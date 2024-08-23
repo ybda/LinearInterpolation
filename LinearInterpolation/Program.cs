8c31278399a5c3c0f5598b349d12886e78591ac7
Dmitriy B. <ybdapw@gmail.com>
2024-08-23 12:39:52 +0500

    toml config

--- LinearInterpolation/Program.cs
+++ LinearInterpolation/Program.cs
@@ -1,35 +1,58 @@
-﻿namespace LinearInterpolation;
+﻿using Tomlyn;
 
-public record Range<T>(T Min, T Max);
+namespace LinearInterpolation;
+
+public record Range<T>(T Min, T Max) where T : new()
+{
+    public Range(IList<T> items) : this(items[0], items[1]) {}
+    public Range() : this(new T(), new T()) {}
+}
+
+public record NamedRange<T>(string Name, Range<T> Range) where T : new()
+{
+    public NamedRange() : this("", new Range<T>()) {}
+}
+
+
+public class TomlModel
+{
+    public NamedRange<decimal> Range1 { get; set; }
+    public NamedRange<decimal> Range2 { get; set; }
+    public decimal Input { get; set; }
+    public int InputRangeNumber { get; set; }
+}
 
 public static class Program
 {
     private static void Main()
     {
-        var range1 = new Range<decimal>(0, 5);
-        const string range1Name = "mA";
+        for (;;)
+        {
+            var cfg = GetTomlTableFromFile();
         
-        var range2 = new Range<decimal>(-200, 110);
-        const string range2Name = "°C";
+            string printString = cfg.InputRangeNumber == 1 ?
+                InterpolationToString(cfg.Input, cfg.Range1, cfg.Range2) 
+                : InterpolationToString(cfg.Input, cfg.Range2, cfg.Range1);
+        
+            Console.WriteLine($"[{DateTime.Now}] {printString}");
+            
+            Console.Write("> ");
+            Console.ReadLine();
+        }
+    }
 
-        const decimal input = -167.0m;
-        const int inputRangeNumber = 2;
-        
-        
-        
-        string printString = inputRangeNumber == 1 ?
-            InterpolationToString(input, range1, range1Name, range2, range2Name) 
-            : InterpolationToString(input, range2, range2Name, range1, range1Name);
-        
-        Console.WriteLine(printString);
+    private static TomlModel GetTomlTableFromFile()
+    {
+        string tomlContent = File.ReadAllText("../../../params.toml");
+        return Toml.Parse(tomlContent).ToModel<TomlModel>();
     }
 
     private static string InterpolationToString(decimal input, 
-        Range<decimal> range1, string range1Name, 
-        Range<decimal> range2, string range2Name)
+        NamedRange<decimal> range1, 
+        NamedRange<decimal> range2)
     {
-        var output = Interpolate(input, range1, range2);
-        return $"{input:F2} {range1Name} = {output:F2} {range2Name}";
+        var output = Interpolate(input, range1.Range, range2.Range);
+        return $"{input:F2} {range1.Name} = {output:F2} {range2.Name}";
     }
     
     public static decimal Interpolate(decimal input, Range<decimal> inputRange, Range<decimal> outputRange)
